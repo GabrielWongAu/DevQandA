2ba58a482dbd7542cb00284bc7430b0cb4c1170f
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-02-24 08:56:12 +1100

    Updated admin.py and models.py

--- main/models.py
+++ main/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Question(models.Model)
+class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     body = models.TextField()
     def __str__(self):
         return self.title
 
-class Answer(models.Model)
+class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
