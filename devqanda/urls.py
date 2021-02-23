78c57ece72da54a742f2b42b356d0f4ee4c43866
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-02-24 08:59:26 +1100

    Changed the URL scheme

--- devqanda/urls.py
+++ devqanda/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('', homePageView),
+    path('about/', aboutPageView),
     path('accounts/', include('allauth.urls')),
 ]
