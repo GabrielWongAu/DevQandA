7767ec74222253031129dae71eeffe29a3e64eca
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-02-24 08:41:40 +1100

    Created Question and Answer models

--- devqanda/settings.py
+++ devqanda/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'main.apps.MainConfig',
         
     'django.contrib.sites', # for allauth
     'allauth',
