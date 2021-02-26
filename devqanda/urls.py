b45c73f3759968b789cff548b288091c1051b228
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-02-27 09:43:06 +1100

    Created a new question page

--- devqanda/urls.py
+++ devqanda/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from main.views import homeFeedView
 from pages.views import aboutPageView
-from questions.views import questionView
+from questions.views import questionView, newView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +25,5 @@ urlpatterns = [
     path('about/', aboutPageView),
     path('accounts/', include('allauth.urls')),
     path('question/<int:id>/', questionView),
+    path('question/new', newView)
 ]
