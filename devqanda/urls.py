6df03c33c23ce824155f762e73ff0baff6b2b8c3
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-02-27 08:30:25 +1100

    Created a template for the question page

--- devqanda/urls.py
+++ devqanda/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from main.views import homeFeedView
 from pages.views import aboutPageView
+from questions.views import questionView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homeFeedView),
     path('about/', aboutPageView),
     path('accounts/', include('allauth.urls')),
+    path('question/<int:id>/', questionView),
 ]
