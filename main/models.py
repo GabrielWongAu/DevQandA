130077d2e957674bc3710342c55b5a562a1266a8
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-02-27 09:31:49 +1100

    Added modified and created timefield in questions

--- main/models.py
+++ main/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     body = models.TextField()
+    created     = models.DateTimeField(editable=False)
+    modified    = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Question, self).save(*args, **kwargs)
     def __str__(self):
         return self.title
 
