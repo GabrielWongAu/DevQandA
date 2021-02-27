24c6c935d7f2ef84886e6691cb3cc7fb3585bebe
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-02-27 11:12:49 +1100

    Created answerView and simplified questionView

--- devqanda/urls.py
+++ devqanda/urls.py
@@ -1,23 +1,8 @@
-"""devqanda URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from main.views import homeFeedView
 from pages.views import aboutPageView
-from questions.views import questionView, newView
+from questions.views import questionView, newView, answerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +10,6 @@ urlpatterns = [
     path('about/', aboutPageView),
     path('accounts/', include('allauth.urls')),
     path('question/<int:id>/', questionView),
-    path('question/new/', newView)
+    path('question/<int:id>/answer', answerView),
+    path('question/new/', newView),
 ]
